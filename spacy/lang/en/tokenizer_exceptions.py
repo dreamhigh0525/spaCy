dreamhigh0525/spaCy

commit 123810b6ded7a0828c1c4c072fcc8b1287a0c5ed

Add "lovin'" to tokenizer exceptions (see #1248)

spacy/lang/en/tokenizer_exceptions.py
@@ -385,6 +385,8 @@ for exc_data in [
     {ORTH: "Ma'am", LEMMA: "madam", NORM: "madam"},
     {ORTH: "o'clock", LEMMA: "o'clock", NORM: "o'clock"},
     {ORTH: "O'clock", LEMMA: "o'clock", NORM: "o'clock"},
+    {ORTH: "lovin'", LEMMA: "love", NORM: "loving"},
+    {ORTH: "Lovin'", LEMMA: "love", NORM: "loving"},
 
     {ORTH: "Mt.", LEMMA: "Mount", NORM: "Mount"},
     {ORTH: "Ak.", LEMMA: "Alaska", NORM: "Alaska"},

commit 063cf6cda549391bd6857c1f96394258385298ff

Add tokenizer exceptions for ing verbs Extend list of tokenizing exceptions introduced in 123810b

spacy/lang/en/tokenizer_exceptions.py
@@ -387,6 +387,21 @@ for exc_data in [
     {ORTH: "O'clock", LEMMA: "o'clock", NORM: "o'clock"},
     {ORTH: "lovin'", LEMMA: "love", NORM: "loving"},
     {ORTH: "Lovin'", LEMMA: "love", NORM: "loving"},
+    {ORTH: "lovin", LEMMA: "love", NORM: "loving"},
+    {ORTH: "Lovin", LEMMA: "love", NORM: "loving"},
+    {ORTH: "havin'", LEMMA: "have", NORM: "having"},
+    {ORTH: "Havin'", LEMMA: "have", NORM: "having"},
+    {ORTH: "havin", LEMMA: "have", NORM: "having"},
+    {ORTH: "Havin", LEMMA: "have", NORM: "having"},
+    {ORTH: "doin'", LEMMA: "do", NORM: "doing"},
+    {ORTH: "Doin'", LEMMA: "do", NORM: "doing"},
+    {ORTH: "doin", LEMMA: "do", NORM: "doing"},
+    {ORTH: "Doin", LEMMA: "do", NORM: "doing"},
+    {ORTH: "goin'", LEMMA: "go", NORM: "going"},
+    {ORTH: "Goin'", LEMMA: "go", NORM: "going"},
+    {ORTH: "goin", LEMMA: "go", NORM: "going"},
+    {ORTH: "Goin", LEMMA: "go", NORM: "going"},
+
 
     {ORTH: "Mt.", LEMMA: "Mount", NORM: "Mount"},
     {ORTH: "Ak.", LEMMA: "Alaska", NORM: "Alaska"},